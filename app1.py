c44cea21ec76e732a4a37dd7f5647b2905df3aeb
Adrian-Wojas <60942671+Adrian-Wojas@users.noreply.github.com>
2024-04-07 17:25:32 +0200

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,17 @@
+
+from flask import Flask
+
+# Create a flask
+app = Flask(__name__)
+
+# Create an API end point
+@app.route('/hello', methods=['GET'])
+def say_hello():
+    return "Hello World"
+
+@app.route('/')
+def say_he():
+    return "Hello z innej strony"
+
+if __name__ == '__main__':
+    app.run()
